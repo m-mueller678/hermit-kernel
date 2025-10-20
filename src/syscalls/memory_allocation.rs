825f46644c190c1043f4eef554ff9ed3cd20965f
m-mueller678 <marcus.mueller.678@gmail.com>
2025-10-20 12:00:04 +0200

    add syscalls for memory managerment

---
+++ src/syscalls/memory_allocation.rs
@@ -0,0 +1,44 @@
+use memory_addresses::{PhysAddr, VirtAddr};
+
+use crate::mm::physicalmem::{allocate_physical, deallocate_physical};
+use crate::mm::virtualmem::{allocate_virtual, deallocate_virtual};
+
+/// Allocate physical memory.
+#[hermit_macro::system]
+#[unsafe(no_mangle)]
+pub extern "C" fn sys_allocate_physical(size: usize, align: usize) -> usize {
+	match allocate_physical(size, align) {
+		Ok(x) => {
+			assert!(!x.is_null());
+			x.as_usize()
+		}
+		Err(_) => 0,
+	}
+}
+
+/// Deallocate physical memory previously allocated with [sys_allocate_physical].
+#[hermit_macro::system]
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn sys_deallocate_physical(addr: usize, size: usize) {
+	unsafe { deallocate_physical(PhysAddr::from(addr), size) };
+}
+
+/// Allocate virtual memory.
+#[hermit_macro::system]
+#[unsafe(no_mangle)]
+pub extern "C" fn sys_allocate_virtual(size: usize, align: usize) -> usize {
+	match allocate_virtual(size, align) {
+		Ok(x) => {
+			assert!(!x.is_null());
+			x.as_usize()
+		}
+		Err(_) => 0,
+	}
+}
+
+/// Deallocate virtual memory previously allocated with [sys_allocate_virtual].
+#[hermit_macro::system]
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn sys_deallocate_virtual(addr: usize, size: usize) {
+	unsafe { deallocate_virtual(VirtAddr::from(addr), size) };
+}
