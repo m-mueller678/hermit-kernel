055d0fa1c9ff18d68ac16c13e9b7661536f6ebb6
m-mueller678 <marcus.mueller.678@gmail.com>
2026-01-04 00:05:20 +0100

    implement physical memory free list

--- src/mm/physical_memory.rs
+++ src/mm/physical_memory.rs
@@ -2,28 +2,45 @@ use alloc::alloc::AllocError;
 use core::mem::MaybeUninit;
 use core::ops::Range;
 
+use free_list::{FreeList, PageLayout, PageRange};
+use hermit_sync::InterruptTicketMutex;
+
 use crate::mm::page_size::PageSize;
 
 pub fn init() {
 	todo!()
 }
-pub fn allocate_contiguous(page_size: PageSize, count: usize) -> Result<usize, AllocError> {
+pub fn allocate_contiguous(_page_size: PageSize, _count: usize) -> Result<usize, AllocError> {
 	todo!()
 }
 /// Allocates all pages or fails
 /// # Safety
 /// start must be multiple of page size
-pub fn allocate_at(page_size: PageSize, start: usize, count: usize) -> Result<(), AllocError> {
+pub fn allocate_at(_page_size: PageSize, _start: usize, _count: usize) -> Result<(), AllocError> {
 	todo!()
 }
 pub fn allocate_multiple(
 	page_size: PageSize,
 	dst: &mut [MaybeUninit<usize>],
-) -> Result<&mut [usize], AllocError> {
-	todo!()
+) -> Result<(), AllocError> {
+	for i in 0..dst.len() {
+		match allocate(page_size) {
+			Ok(x) => {
+				dst[i].write(x);
+			}
+			Err(_) => unsafe {
+				deallocate_multiple(page_size, dst[..i].assume_init_ref());
+			},
+		}
+	}
+	Ok(())
 }
 pub fn allocate(page_size: PageSize) -> Result<usize, AllocError> {
-	todo!()
+	FREE_LIST
+		.lock()
+		.allocate(PageLayout::from_size_align(page_size.usize(), page_size.usize()).unwrap())
+		.map(|x| x.start())
+		.map_err(|_| AllocError)
 }
 pub unsafe fn deallocate_multiple(page_size: PageSize, frames: &[usize]) {
 	for &x in frames {
@@ -31,9 +48,21 @@ pub unsafe fn deallocate_multiple(page_size: PageSize, frames: &[usize]) {
 	}
 }
 pub unsafe fn deallocate(page_size: PageSize, frame: usize) {
-	todo!()
+	unsafe {
+		FREE_LIST
+			.lock()
+			.deallocate(PageRange::from_start_len(frame, page_size.usize()).unwrap())
+			.unwrap();
+	}
 }
 
 pub unsafe fn claim(range: Range<usize>) {
-	todo!()
+	unsafe {
+		FREE_LIST
+			.lock()
+			.deallocate(PageRange::new(range.start, range.end).unwrap())
+			.unwrap();
+	}
 }
+
+static FREE_LIST: InterruptTicketMutex<FreeList<32>> = InterruptTicketMutex::new(FreeList::new());
