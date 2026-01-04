77b793b5fb54cb3bb99eff7b32f4396613ea5464
m-mueller678 <marcus.mueller.678@gmail.com>
2026-01-04 16:53:59 +0100

    work around free-list overflow bug

--- src/mm/page_size.rs
+++ src/mm/page_size.rs
@@ -22,12 +22,31 @@ impl PageSize {
 	pub fn log2(self) -> u32 {
 		self.0.trailing_zeros()
 	}
+
+	fn low_mask(self) -> usize {
+		self.usize() - 1
+	}
 }
 impl core::ops::Mul<usize> for PageSize {
 	type Output = usize;
 
 	fn mul(self, rhs: usize) -> Self::Output {
 		self.0 * rhs
+	}
+}
+impl core::ops::Mul<PageSize> for usize {
+	type Output = usize;
+
+	fn mul(self, rhs: PageSize) -> Self::Output {
+		self * rhs.0
+	}
+}
+
+impl core::ops::Div<PageSize> for usize {
+	type Output = usize;
+
+	fn div(self, rhs: PageSize) -> Self::Output {
+		self >> rhs.log2()
 	}
 }
 
@@ -40,5 +59,16 @@ impl PageSize {
 	}
 	pub fn size_index(self) -> usize {
 		Arch::page_size_index(self)
+	}
+	pub fn ps_div_ceil(self, x: usize) -> usize {
+		let up = x & self.low_mask() != 0;
+		(x >> self.log2()) + up as usize
+	}
+	pub fn ps_align_down(self, x: usize) -> usize {
+		x & (usize::MAX << self.log2())
+	}
+	pub fn ps_align_up(self, x: usize) -> usize {
+		let mask = self.low_mask();
+		if x & mask == 0 { x } else { (x + mask) & !mask }
 	}
 }
--- src/mm/virtual_memory.rs
+++ src/mm/virtual_memory.rs
@@ -11,10 +11,13 @@ pub fn init() {}
 
 /// Attempts to allocate a range of memory in page granularity.
 pub fn allocate(page_size: PageSize, count: NonZeroUsize) -> Result<NonZeroUsize, AllocError> {
-	match FREE_LISTS[page_size.size_index()]
-		.lock()
-		.allocate(PageLayout::from_size_align(page_size * count.get(), page_size.usize()).unwrap())
-	{
+	match FREE_LISTS[page_size.size_index()].lock().allocate(
+		dbg!(PageLayout::from_size_align(
+			dbg!(page_size) * dbg!(count.get()),
+			page_size.usize()
+		))
+		.unwrap(),
+	) {
 		Ok(x) => Ok(NonZeroUsize::new(x.start()).unwrap()),
 		Err(_) => Err(AllocError),
 	}
@@ -37,12 +40,19 @@ pub unsafe fn deallocate(page_size: PageSize, start: NonZeroUsize, count: NonZer
 		FREE_LISTS[page_size.size_index()]
 			.lock()
 			.deallocate(PageRange::from_start_len(start.get(), page_size * count.get()).unwrap())
-			.unwrap()
+			.unwrap();
 	}
 }
 
 pub unsafe fn claim_pages(page_size: PageSize, start: NonZeroUsize, count: NonZeroUsize) {
-	unsafe { deallocate(page_size, start, count) }
+	// TODO: free-list has a bug with handling usive overflows, stay away from the end of the address space as a work around.
+	// 1<<42 is an arbitrary large power of two.
+	let max_end = 0usize.wrapping_sub(1 << 42);
+	if max_end <= start.get() {
+		return;
+	}
+	let max_count = NonZeroUsize::new((max_end - start.get()) / page_size).unwrap();
+	unsafe { deallocate(page_size, start, count.min(max_count)) }
 }
 
 static FREE_LISTS: [InterruptTicketMutex<FreeList<32>>; NUM_PAGE_SIZES] =
